3ac26e05d6ab8759691e15091a5e623196e453b5
matteojose3112-bit <matteo.jose3112@gmail.com>
2026-08-20 19:41:14 -0400

    Add Experience section to Aster page

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/layout/Navbar";
 import Hero from "./components/layout/sections/Hero";
 import About from "./components/layout/sections/About";
 import Projects from "./components/layout/sections/Projects";
+import Experience from "./components/layout/sections/Experience";
 import Contact from "./components/layout/sections/Contact";
 import Footer from "./components/layout/Footer";
 
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Experience />
       <Contact />
       <Footer />
     </main>
